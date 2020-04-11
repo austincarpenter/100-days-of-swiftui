985db6475da4a80c1c8da2330d940271b91bc0d2
Austin Carpenter <austincarpenter@Austins-MacBook-Pro.local>
2020-04-11 10:49:40 +1000

    Day 1

---
+++ Day 1.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+//"Variables"
+var str = "Hello, playground"
+str = "Hello, playground. Austin here."
+
+//"Strings and integers"
+var age = 100
+var populationAustralia = 25_434_405
+
+//"Multi-line strings"
+var str1 = """
+This string
+goes over multiple
+lines. How cool!
+"""
+
+var str2 = """
+This string \
+goes over multiple \
+lines. This one won't \
+actually include \
+the line breaks though.
+"""
+
+//"Doubles and booleans"
+var e = 2.718
+var hotToday = true
+
+//String Interpolation
+var score = 100
+var scoreString = "Your score was \(score)"
+var results = "Results are in! \(scoreString)"
+
+//"Constants"
+let swift = "fun"
+
+//"Type Annotations"
+let storeName: String = "Woolworths"
+let streetNumber: Int = 30
+let applePrice: Double = 1.50
+let hasRestockedToiletPaper: Bool = true
