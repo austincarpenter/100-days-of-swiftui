2460e0ef4d4c75ae7d08a1c051c810ddb0c0064a
Austin Carpenter <developer@austincarpenter.com.au>
2020-06-28 15:20:22 +1000

    Added accessibility affordances.

--- Starting SwiftUI/GuessTheFlag/GuessTheFlag/ContentView+Challenges.swift
+++ Starting SwiftUI/GuessTheFlag/GuessTheFlag/ContentView+Challenges.swift
@@ -8,16 +8,6 @@
 
 import SwiftUI
 
-struct FlagImage: View {
-    var name: String
-    var body: some View {
-        Image(name)
-            .renderingMode(.original)
-            .cornerRadius(20, antialiased: true)
-            .shadow(color: .black, radius: 2)
-    }
-}
-
 struct ContentView_Challenges: View {
     
     @State private var showingScore = false
@@ -25,6 +15,21 @@ struct ContentView_Challenges: View {
     @State private var score = 0
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     @State private var correctAnswer = Int.random(in: 0...2)
     
     @State private var correctYRotation = 0.0
@@ -50,7 +55,11 @@ struct ContentView_Challenges: View {
                         self.setButtonStates(correctAnswer: number == self.correctAnswer)
                         self.flagTapped(number)
                     }) {
-                        FlagImage(name: self.countries[number])
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .cornerRadius(20, antialiased: true)
+                            .shadow(color: .black, radius: 2)
+                            .accessibility(label: Text(self.labels[self.countries[number], default: "Unknown flag"]))
                     }
                     .rotation3DEffect(number == self.correctAnswer ? .degrees(self.correctYRotation) : .zero, axis: (x: 0, y: 1, z: 0))
                     .opacity(number == self.correctAnswer ? 1.0 : self.incorrectOpacity)
